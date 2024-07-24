9c2ee86c5c7a8c4502bed2dd27513be3ce3064fa
Oleg <olegpanin1968@gmail.com>
2024-07-24 23:33:32 +0300

    change the creation of the  object map

--- src/app/DataHandler.java
+++ src/app/DataHandler.java
@@ -5,20 +5,10 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class DataHandler {
 
-  /*  Створення об'єкта за допомогою new DataRepository()
-  і присвоєння його змінній типу Map<Integer, String> неможливе
-   через те, що DataRepository не є класом, який реалізує
-    інтерфейс Map. DataRepository - це клас, який містить метод getData(),
-     що повертає об'єкт типу Map<Integer, String>.
-   */
 
-    private final Map<Integer, String> map;
 
-    // Конструктор, який ініціалізує карту
-    public DataHandler() {
-        DataRepository repository = new DataRepository();
-        this.map = repository.getData();
-    }
+    private final  Map<Integer, String> map = new DataRepository().getData();
+
 
     // Метод формує виведення нумерованого переліку імен
     public String getAll() {
